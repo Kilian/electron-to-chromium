8413ba2f1f675580db1047defa490ce29e2d7192
kilian <kilian@kilianvalkhof.com>
2017-04-08 21:34:07 +0200

    test.js formatting and naming

--- test.js
+++ test.js
@@ -6,18 +6,16 @@ test('test electronToChromium', t => {
   t.is(e2c.electronToChromium(1), "49");
   t.is(e2c.electronToChromium(1.6), "56");
   t.is(e2c.electronToChromium("1.6"), "56");
-	t.is(e2c.electronToChromium("1.5.1"), "54.0.2840.101");
-	t.is(e2c.electronToChromium("9000"), undefined);
+  t.is(e2c.electronToChromium("1.5.1"), "54.0.2840.101");
+  t.is(e2c.electronToChromium("9000"), undefined);
 });
-
 test('test chromiumToElectron', t => {
   t.is(e2c.chromiumToElectron(1), undefined);
   t.is(e2c.chromiumToElectron(56), "1.6");
   t.is(e2c.chromiumToElectron("56"), "1.6");
   t.deepEqual(e2c.chromiumToElectron("54.0.2840.51"), ["1.4.12"]);
-	t.is(e2c.chromiumToElectron("9000"), undefined);
+  t.is(e2c.chromiumToElectron("9000"), undefined);
 });
-
 test('test electronToBrowserList', t => {
   t.is(e2c.electronToBrowserList("1.6"), "Chrome >= 56");
   t.is(e2c.electronToBrowserList(9999), undefined);
@@ -40,15 +38,15 @@ test('test fullChromiumVersions', t => {
 
 
 // test individial includes
-test('test versions', t => {
+test('test versions.js', t => {
   t.is(require("./versions")["1.6"], "56");
 });
-test('test fullVersions', t => {
+test('test full-versions.js', t => {
   t.is(require("./full-versions")["1.5.1"], "54.0.2840.101");
 });
-test('test chromiumVersions', t => {
+test('test chromium-versions.js', t => {
   t.is(require("./chromium-versions")["56"], "1.6");
 });
-test('test fullChromiumVersions', t => {
+test('test full-chromium-versions.js', t => {
   t.deepEqual(require("./full-chromium-versions")["54.0.2840.51"], ["1.4.12"]);
 });
