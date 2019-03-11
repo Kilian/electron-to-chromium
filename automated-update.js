7080d8ea1bca9d4c490e396ca6e1fbc22a3bd855
kilian <kilian@kilianvalkhof.com>
2019-03-11 10:18:40 +0100

    include package.json and package-lock.json files in commit for regeneration (electron-releases could be updated, updating these files)

--- automated-update.js
+++ automated-update.js
@@ -21,7 +21,7 @@ exec('npm update electron-releases && npm run build && npm test', {silent:true},
     exec('git status --porcelain', {silent:true}, function(code, stdout, stderr) {
       const indexHasUpdated = !!stdout.length;
       if(indexHasUpdated) {
-        exec('git add versions.js full-versions.js chromium-versions.js full-chromium-versions.js', {silent:true});
+        exec('git add versions.js full-versions.js chromium-versions.js full-chromium-versions.js package.json package-lock.json', {silent:true});
         exec('git commit -m "generate new version"', {silent:true});
         exec('npm version patch', {silent:true});
         exec('git push origin master', {silent:true});
