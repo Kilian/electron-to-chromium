53b15968c159a3d4bd0d43481d6b6b11986605a2
Norbert <78374441+xkotori@users.noreply.github.com>
2022-05-06 23:32:20 +0200

    Refactor main code to support future versions

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-var versions = require('./versions');
-var fullVersions = require('./full-versions');
-var chromiumVersions = require('./chromium-versions');
-var fullChromiumVersions = require('./full-chromium-versions');
+const versions = require('./versions'),
+      fullVersions = require('./full-versions'),
+      chromiumVersions = require('./chromium-versions'),
+      fullChromiumVersions = require('./full-chromium-versions');
 
-var electronToChromium = function (query) {
-  var number = getQueryString(query);
+const electronToChromium = query => {
+  const number = getQueryString(query);
   return number.split('.').length > 2 ? fullVersions[number] : versions[number] || undefined;
 };
 
-var chromiumToElectron = function (query) {
-  var number = getQueryString(query);
+const chromiumToElectron = query => {
+  const number = getQueryString(query);
   return number.split('.').length > 2 ? fullChromiumVersions[number] : chromiumVersions[number] || undefined;
 };
 
-var electronToBrowserList = function (query) {
+const electronToBrowserList = query => {
   var number = getQueryString(query);
   return versions[number] ? "Chrome >= " + versions[number] : undefined;
 };
 
-var getQueryString = function (query) {
-  var number = query;
+const getQueryString = query => {
+  let number = query;
   if (query === 1) { number = "1.0" }
   if (typeof query === 'number') { number += ''; }
   return number;
