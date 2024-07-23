d7636ff40320480c9a045f1fcf491342911e44cb
Kilian Valkhof <kilian@kilianvalkhof.com>
2024-07-23 09:37:22 +0200

    ignore all nightly version

--- build.mjs
+++ build.mjs
@@ -18,19 +18,19 @@ await fetch("https://electronjs.org/headers/index.json")
 
         if (!electron.version.includes("nightly")) {
           // simple list
-          const simpleVersion =  electron.version.split(".")[0] + "." + electron.version.split(".")[1];
+          const simpleVersion = electron.version.split(".")[0] + "." + electron.version.split(".")[1];
           const chromeVersion = electron.chrome.split(".")[0];
           electronVersions[simpleVersion] = chromeVersion;
           chromiumVersions[chromeVersion] = chromiumVersions[chromeVersion] || simpleVersion;
-        }
 
-        // explicit list
-        electronFullVersions[electron.version] = electron.chrome;
-        if (!chromiumFullVersions[electron.chrome]) {
-          chromiumFullVersions[electron.chrome] = [];
+          // explicit list
+          electronFullVersions[electron.version] = electron.chrome;
+          if (!chromiumFullVersions[electron.chrome]) {
+            chromiumFullVersions[electron.chrome] = [];
+          }
+          chromiumFullVersions[electron.chrome].push(electron.version);
         }
-        chromiumFullVersions[electron.chrome].push(electron.version);
-    });
+      });
 
     [
       {list: electronVersions, file: "versions"},
